d2aeff28241ad57bfc67be3520b67bcf88874c48
damiandattilo1 <60588905+damiandattilo1@users.noreply.github.com>
2020-02-10 09:22:22 -0300

    Add files via upload

---
+++ 2-InstruccionIf/2-InstruccionIf/jsInstruccionIF (1).js
@@ -0,0 +1,16 @@
+function mostrar()
+{
+//tomo la edad  
+
+    var num;
+    
+    num = document.getElementById("edad").value;
+
+    parseInt(num);
+
+    if(num == 15)
+    {
+        alert("Niña bonita");
+    }
+
+}//FIN DE LA FUNCIÓN
