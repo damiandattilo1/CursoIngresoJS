5263110b9d24101a894980edb81cc99bde250321
damiandattilo1 <60588905+damiandattilo1@users.noreply.github.com>
2020-02-21 10:43:51 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,7 +3,26 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var seguir;
+	var numero;
+	
+	do
+	{
+		numero=parseInt(prompt("Ingrese un numero"));
+		while(isNaN(numero))
+		{
+			numero=parseInt(prompt("Eso no es un numero. Ingrese un numero"));
+		}
+		acumulador=acumulador+numero;
+
+		contador = contador + 1;
+
+		seguir=prompt("Quiere ingresar otro numero?");
+	}while(seguir=='s');
+
+	promedio = acumulador / contador;
+
+	alert(("La suma es ") + acumulador + (" y el promedio es ") + promedio);
 
 
 document.getElementById('suma').value=acumulador;
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,8 +4,27 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
+	var numero;
 	
 	var respuesta='si';
+
+	do
+	{
+		numero=parseInt(prompt("Ingrese un numero"));
+		while(isNaN(numero))
+		{
+			numero=parseInt(prompt("Eso no es un numero. Ingrese un numero"));
+		}
+		
+		if(numero>=0)
+		{
+			suma = s
+		}
+
+		contador = contador + 1;
+
+		seguir=prompt("Quiere ingresar otro numero?");
+	}while(seguir=='s');
 
 
 document.getElementById('suma').value=positivo;
