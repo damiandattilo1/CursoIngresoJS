a4401845379347abd82d397b9ee4a7eda5409329
damiandattilo1 <60588905+damiandattilo1@users.noreply.github.com>
2020-02-28 10:21:04 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,10 @@
 function mostrar()
 {
+    var num=1;
 
+    for(i=0;i<10;i++)
+    {
+        document.write(num + " ");
+        num++;
+    }
 }
--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,7 +1,12 @@
 function mostrar()
 {
+    var i;
+    var repeticiones = parseInt(prompt("ingrese el número de repeticiones"));
 
-var repetciones = prompt("ingrese el número de repeticiones");
+    for(i=0;i<repeticiones;i++)
+    {
+        document.write("Hola UTN FRA </br>");
+    }
 
 
 
