15b3fb23820d298e2f95dc38299811ef45b5178c
damiandattilo1 <60588905+damiandattilo1@users.noreply.github.com>
2020-02-28 10:11:39 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -12,7 +12,17 @@ function mostrar()
 	
 	}
 
-
+/*
+	1-Suma de los negativos.
+	2-Suma de los positivos.
+	3-Cantidad de positivos. 
+	4-Cantidad de negativos. 
+	5-Cantidad de ceros. 
+	6-Cantidad de números pares. 
+	7-Promedio de positivos. 
+	8-Promedios de negativos. 
+	9-Diferencia entre positivos y negativos, (positvos-negativos).
+*/
 
 
 }//FIN DE LA FUNCIÓN
